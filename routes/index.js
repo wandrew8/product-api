bf69f9d366cf35a656f4fca8f0a0477259a884b1
Andrew Weiss <wandrew8@gmail.com>
2020-10-09 23:31:17 -0700

    adds handlebars html templating for admin site

--- routes/index.js
+++ routes/index.js
@@ -7,7 +7,7 @@ router.use(bodyParser.urlencoded({ extended: true }));
 router.use("/products", require('./products'));
 
 router.use("/", async (req, res, next) => {
-  res.send("Welcome to the World Treats Products API")
+  res.render("home")
 });
 
 
--- server.js
+++ server.js
@@ -1,7 +1,14 @@
 const express = require("express");
 const routes = require("./routes");
 const server = express();
+const exphbs = require('express-handlebars');
 
+server.engine('hbs', exphbs({
+    defaultLayout: 'main',
+    extname: '.hbs'
+}));
+
+server.set('view engine', 'hbs');
 server.use(express.json());
 server.use(express.static(__dirname + '/public'));
 server.use(routes);
