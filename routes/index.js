e3116c9e919d0c084ae9eb2554e7ce63c9af9b81
Andrew Weiss <wandrew8@gmail.com>
2021-02-18 17:33:01 -0800

    adds cors package to fix front end cors issues

--- routes/index.js
+++ routes/index.js
@@ -2,9 +2,10 @@ const { Router } = require("express");
 const router = Router();
 const bodyParser = require('body-parser');
 const productDAO = require("../daos/product");
+const cors = require("cors");
 
 router.use(bodyParser.urlencoded({ extended: true }));
-
+router.use(cors());
 router.use("/products", require('./products'));
 
 router.use("/all", async (req, res, next) => {
